0ef90dc2c325ba5e90cd32d3dc201473aba7be29
vasusakthi008-max <vasusakthi008@gmail.com>
2026-09-05 22:56:22 +0530

    updated session_3

---
+++ Assignment_problems/ProductExceptSelf.java
@@ -0,0 +1,34 @@
+public class ProductExceptSelf {
+
+    static int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        int left = 1;
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = left;
+            left = left * nums[i];
+        }
+
+        int right = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * right;
+            right = right * nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        for (int x : result)
+            System.out.print(x + " ");
+    }
+}
