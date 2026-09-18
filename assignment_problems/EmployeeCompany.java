39627f1e6ad2a81a8039435d9a9b73b5049b417a
gaakash7-ship <gaakashg7@gmial.com>
2026-09-18 22:30:00 +0530

    updated session_5

--- assignment_problems/EmployeeCompany.java
+++ assignment_problems/EmployeeCompany.java
@@ -4,18 +4,15 @@ class Employee {
     String empName;
     double salary;
 
-    // Static fields
     static String companyName = "Bright Horizon Technologies";
     static int employeeCount = 0;
 
-    // Constructor
     Employee(String empName, double salary) {
         this.empName = empName;
         this.salary = salary;
         employeeCount++;
     }
 
-    // Static method
     static void printCompanyInfo() {
         System.out.println(companyName);
         System.out.println("Employees on record: " + employeeCount);
@@ -25,12 +22,10 @@ class Employee {
 public class EmployeeCompany {
     public static void main(String[] args) {
 
-        // Create three Employee objects
         Employee e1 = new Employee("Arjun", 30000);
         Employee e2 = new Employee("Priya", 35000);
         Employee e3 = new Employee("Divya", 40000);
 
-        // Call static method using class name
         Employee.printCompanyInfo();
     }
 }
