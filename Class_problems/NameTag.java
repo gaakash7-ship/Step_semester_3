75179b176006af6aa2e6ee98f10d3f33d4a5e581
gaakash7-ship <gaakashg7@gmial.com>
2026-09-25 15:48:28 +0530

    updated session_6

---
+++ Class_problems/NameTag.java
@@ -0,0 +1,23 @@
+package Class_problems;
+
+class NameTag {
+    private final String firstName;
+    private final String lastName;
+
+    NameTag(String fullName) {
+        String[] parts = fullName.split(" ");
+
+        firstName = parts[0];
+        lastName = parts[1];
+    }
+
+    public String getNickname() {
+        return firstName + " " + lastName.charAt(0) + ".";
+    }
+
+    public static void main(String[] args) {
+        NameTag tag = new NameTag("Maria Gomez");
+
+        System.out.println("Nickname: " + tag.getNickname());
+    }
+}
